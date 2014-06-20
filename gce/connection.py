82d12dbab02d725b438e7f70c2cde707064d787a
Naveen <naviensubramani@gmail.com>
2014-06-20 17:22:10 +0530

    fixes

--- gce/connection.py
+++ gce/connection.py
@@ -9,17 +9,18 @@ from oauth2client.file import Storage
 from oauth2client.tools import run
 from apiclient.discovery import build
 
-from boto.gce.instance import Instance
+from gce.instance import Instance
 
 
 OAUTH2_STORAGE = 'oauth2.dat'
 GCE_SCOPE = 'https://www.googleapis.com/auth/compute'
 API_VERSION = 'v1'
+DEFAULT_ZONE = 'us-central1-a'
 
 
 class GCEConnection(object):
 
-    def __init__(self, client_secrets, project_id):
+    def __init__(self, client_secrets, project_id, zone=None):
         logging.basicConfig(level=logging.INFO)
 
         self.project_id = project_id
@@ -27,6 +28,10 @@ class GCEConnection(object):
         self.credentials = self.storage.get()
         self.default_network = "https://www.googleapis.com/compute/{0}/projects/google/networks/default".format(API_VERSION)
         self.gce_url = 'https://www.googleapis.com/compute/{0}/projects'.format(API_VERSION)
+        if zone is None:
+            self.zone = DEFAULT_ZONE
+        else:
+            self.zone = zone
 
         # Perform OAuth 2.0 authorization.
         flow = flow_from_clientsecrets(client_secrets, scope=GCE_SCOPE)
@@ -39,17 +44,20 @@ class GCEConnection(object):
 
     # Instance Operations
 
-    def get_all_instances(self, default_zone, list_filter=None):
+    def get_all_instances(self, list_filter=None):
         """
         Retrieve all the Google Compute Engine instances available to specific
         project.
         """
         list_gce_instances = self.service.instances().list(
-            project=self.project_id, filter=list_filter, zone=default_zone).execute(http=self.http)
+            project=self.project_id, filter=list_filter, zone=self.zone).execute(http=self.http)
+
+        print list_gce_instances
 
         list_instances = []
-        for instance in list_gce_instances['items']:
-            list_instances.append(Instance(instance))
+        if 'items' in list_gce_instances:
+            for instance in list_gce_instances['items']:
+                list_instances.append(Instance(instance))
 
         return list_instances
 
@@ -74,7 +82,7 @@ class GCEConnection(object):
              }]
         }
 
-        gce_instance = self.service.instances().insert(project=self.gce_project,
+        gce_instance = self.service.instances().insert(project=self.project_id,
                                                        body=body, zone=zone_name).execute(
                                                            http=self.http)
         return Instance(gce_instance)
@@ -90,6 +98,12 @@ class GCEConnection(object):
         :rtype: :class:`boto.gce.operation.Operation`
         :return: A Google Compute Engine operation.
         """
-        gce_instance = self.service.instances().delete(project=self.gce_project,
-                                                       instance=name).execute(
+        gce_instance = self.service.instances().delete(project=self.project_id,
+                                                       instance=name, zone=self.zone).execute(
                                                            http=self.http)
+        return gce_instance
+
+    def reboot_instance(self):
+        pass
+
+
--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ __author__ = 'naveen'
 from setuptools import setup
 
 setup(
-    name="oneclick",
-    version="1.0",
-    description = "OneClick Installation for Hadoop",
+    name="gce_boto",
+    version="0.1",
+    description = "boto library for Google Compute Engine",
     author = "Naveen Subramani",
     author_email = "naviensubramani@gmail.com",
     install_requires=[
